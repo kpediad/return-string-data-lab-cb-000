4058c174f4d4281870b465ab87103bf1d3854816
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-06-16 13:38:00 +0000

    Automatically backed up by Learn

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,7 +7,6 @@ require 'spec_helper'
 require 'rspec/rails'
 require 'capybara/poltergeist'
 Capybara.javascript_driver = :poltergeist
-#Capybara.default_max_wait_time = 10
 # Add additional requires below this line. Rails is not loaded until this point!
 
 if RUBY_VERSION>='2.6.0'
