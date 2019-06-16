e3bc5e00df1f3e840e82533aa8efa57c65fb5904
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-06-16 11:34:53 +0000

    Automatically backed up by Learn

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,10 +3,6 @@ class Product < ActiveRecord::Base
   has_many :orders, :through => :ordered_products
 
   def is_available?
-<<<<<<< HEAD
     self.inventory > 0 ? 'true' : 'false'
-=======
-    self.inventory ? 'true' : 'false'
->>>>>>> 978e883bef88b8be0e5c671c5f7578e2f2b2e077
   end
 end
